936afb3895a88631c3d80bb5158813de57d54bb3
Joaquin Morales <joaquin.moralestr@gmail.com>
2023-02-21 18:59:37 -0300

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Joaquin Yapp on 21-02-23.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
